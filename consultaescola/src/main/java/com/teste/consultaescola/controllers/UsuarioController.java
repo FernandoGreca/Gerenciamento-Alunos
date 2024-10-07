664ff448760d14871c0765f37a5722ffb8743b08
Fernando Greca <144340988+FernandoGreca@users.noreply.github.com>
2024-10-07 16:38:32 -0300

    Página Cadastro Não funciona nem com reza

--- consultaescola/src/main/java/com/teste/consultaescola/controllers/UsuarioController.java
+++ consultaescola/src/main/java/com/teste/consultaescola/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
+import com.teste.consultaescola.model.Usuario;
 
 @Controller
 public class UsuarioController {
@@ -15,4 +16,13 @@ public class UsuarioController {
         mv.setViewName("login/login");
         return mv;
     }
+
+    @GetMapping("/cadastro")
+    public ModelAndView cadastro() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("login/cadastro");
+        mv.addObject("aluno", new Usuario());
+        return mv;
+    }
+    
 }
