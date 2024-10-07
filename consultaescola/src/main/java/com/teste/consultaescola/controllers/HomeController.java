c9fce5611a47ed698de2bb501d45bec1450cbf86
Fernando Greca <144340988+FernandoGreca@users.noreply.github.com>
2024-10-07 13:47:17 -0300

    Modal de pesquisa feito (front-end)

--- consultaescola/src/main/java/com/teste/consultaescola/controllers/HomeController.java
+++ consultaescola/src/main/java/com/teste/consultaescola/controllers/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.teste.consultaescola.model.Aluno;
+
 
 @Controller
 public class HomeController {
@@ -12,6 +14,7 @@ public class HomeController {
     public ModelAndView index() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("home/index");
+        mv.addObject("aluno", new Aluno());
         return mv;
     }
     
